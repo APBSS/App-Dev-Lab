721707943e49595b24430f73e47f44c7b3427922
bala227 <115527760+bala227@users.noreply.github.com>
2024-07-16 05:58:26 +0000

    first

---
+++ gym/src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../components/Login.css'
+export default function Login() {
+  return (
+    <div className='login'>
+        <nav className='loginnav'>
+          <ul>
+            <li className='title'>GYM</li>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+    </div>
+
+  )
+}
