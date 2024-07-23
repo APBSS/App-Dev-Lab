486cdbf81cf215663b2ea028e13ce713582ba215
bala227 <115527760+bala227@users.noreply.github.com>
2024-07-23 05:29:18 +0000

    S

--- gym/src/components/Navbar.jsx
+++ gym/src/components/Navbar.jsx
@@ -6,10 +6,12 @@ export default function Navbar() {
     <div className='navbar'>
         <nav className='nav'>
           <ul>
-            <li className='title'>GYM</li>
+            <h1 className='title'>Gains<span style={{color:"white"}}>+</span></h1>
             <li>Home</li>
             <li>About</li>
+            <li>Services</li>
             <li>Contact Us</li>
+            <p className='button'>Sign in</p>
           </ul>
         </nav>
     </div>
