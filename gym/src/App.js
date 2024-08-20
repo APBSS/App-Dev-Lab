07e2e4e0923fb0b1e5ab28a1a0e256a7d62034c1
Ashwin-001 <ashwinkumarrs13@gmail.com>
2024-08-20 12:00:42 +0530

    few button changes and dashboard inclusion

--- gym/src/App.js
+++ gym/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import SignIn from "./components/SignIn";
 import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/signin" element={<SignIn />}/>
         <Route path="/register" element={<Register />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
       </Routes>
     </BrowserRouter>
   );
